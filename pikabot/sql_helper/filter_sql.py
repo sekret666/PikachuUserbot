92188a12ca9fdfe13cbfced0d5495e0efa1893e6
Sachin Jangir <38481181+ItzSjDude@users.noreply.github.com>
2020-09-27 16:44:05 +0530

    Update filter_sql.py

--- pikabot/sql_helper/filter_sql.py
+++ pikabot/sql_helper/filter_sql.py
@@ -29,6 +29,32 @@ class Filters(BASE):
 
 Filters.__table__.create(checkfirst=True)
 
+class Filterx(BASE):
+    __tablename__ = "filterx"
+    chat_id = Column(String(14), primary_key=True)
+    keyword = Column(UnicodeText, primary_key=True)
+    reply = Column(UnicodeText)
+    snip_type = Column(Numeric)
+    media_id = Column(UnicodeText)
+    media_access_hash = Column(UnicodeText)
+    media_file_reference = Column(LargeBinary)
+
+    def __init__(
+        self,
+        chat_id,
+        keyword, reply, snip_type,
+        media_id=None, media_access_hash=None, media_file_reference=None
+    ):
+        self.chat_id = chat_id
+        self.keyword = keyword
+        self.reply = reply
+        self.snip_type = snip_type
+        self.media_id = media_id
+        self.media_access_hash = media_access_hash
+        self.media_file_reference = media_file_reference
+
+
+Filterx.__table__.create(checkfirst=True)
 
 def get_filter(chat_id, keyword):
     try:
@@ -72,6 +98,52 @@ def remove_filter(chat_id, keyword):
 
 def remove_all_filters(chat_id):
     saved_filter = SESSION.query(Filters).filter(Filters.chat_id == str(chat_id))
+    if saved_filter:
+        saved_filter.delete()
+        SESSION.commit()
+
+def gt_f(chat_id, keyword):
+    try:
+        return SESSION.query(Filterx).get((str(chat_id), keyword))
+    except:
+        return None
+    finally:
+        SESSION.close()
+
+
+def gt_af(chat_id):
+    try:
+        return SESSION.query(Filterx).filter(Filterx.chat_id == str(chat_id)).all()
+    except:
+        return None
+    finally:
+        SESSION.close()
+
+
+def adf(chat_id, keyword, reply, snip_type, media_id, media_access_hash, media_file_reference):
+    adder = SESSION.query(Filterx).get((str(chat_id), keyword))
+    if adder:
+        adder.reply = reply
+        adder.snip_type = snip_type
+        adder.media_id = media_id
+        adder.media_access_hash = media_access_hash
+        adder.media_file_reference = media_file_reference
+    else:
+        adder = Filters(chat_id, keyword, reply, snip_type, media_id,
+                        media_access_hash, media_file_reference)
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def rm_fl(chat_id, keyword):
+    saved_filter = SESSION.query(Filterx).get((str(chat_id), keyword))
+    if saved_filter:
+        SESSION.delete(saved_filter)
+        SESSION.commit()
+
+
+def rm_af(chat_id):
+    saved_filter = SESSION.query(Filterx).filter(Filterx.chat_id == str(chat_id))
     if saved_filter:
         saved_filter.delete()
         SESSION.commit()
