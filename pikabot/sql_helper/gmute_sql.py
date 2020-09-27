69bab0cadd94ce821c09372b81cead6921b09334
Sachin Jangir <38481181+ItzSjDude@users.noreply.github.com>
2020-09-27 19:43:25 +0530

    Update gmute_sql.py

--- pikabot/sql_helper/gmute_sql.py
+++ pikabot/sql_helper/gmute_sql.py
@@ -1,3 +1,9 @@
+# © 2020 PikaBot
+#
+# You may not use this file without proper authorship and consent from @ItzSjDudeSupport
+#
+# Made by @ItzSjDude for Pikabot
+
 try:
     from pikabot.sql_helper import SESSION, BASE
 except ImportError:
@@ -34,6 +40,38 @@ def gmute(sender):
 
 def ungmute(sender):
     rem = SESSION.query(GMute).get((str(sender)))
+    if rem:
+        SESSION.delete(rem)
+        SESSION.commit()
+
+class GMute2(BASE):
+    __tablename__ = "gmute2"
+    sender = Column(String(14), primary_key=True)
+
+    def __init__(self, sender):
+        self.sender = str(sender)
+
+
+GMute2.__table__.create(checkfirst=True)
+
+
+def is_gmuted2(sender_id):
+    try:
+        return SESSION.query(GMute2).all()
+    except:
+        return None
+    finally:
+        SESSION.close()
+
+
+def gmute2(sender):
+    adder = GMute2(str(sender))
+    SESSION.add(adder)
+    SESSION.commit()
+
+
+def ungmute2(sender):
+    rem = SESSION.query(GMute2).get((str(sender)))
     if rem:
         SESSION.delete(rem)
         SESSION.commit()
