a8f5395c05092a5168579efba453a0e9cdc1021f
Sachin Jangir <38481181+ItzSjDude@users.noreply.github.com>
2020-09-29 12:01:39 +0530

    Update __main__.py

--- pikabot/__main__.py
+++ pikabot/__main__.py
@@ -73,7 +73,7 @@ for name in files:
     with open(name) as f:
         path1 = Path(f.name)
         shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
+        self.load_module(shortname.replace(".py", ""))
 
 
    
