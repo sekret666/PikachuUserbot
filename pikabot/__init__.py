299979059f8c27dd4f7e54c22ce7d68a6caffcce
Sachin Jangir <support@ItzSjDude.in>
2020-10-03 23:22:11 +0530

    Update __init__.py

--- pikabot/__init__.py
+++ pikabot/__init__.py
@@ -3,6 +3,18 @@ from logging import basicConfig, getLogger, INFO, DEBUG ; from distutils.util im
 from pySmartDL import SmartDL
 from requests import get
 import shutil
+shutil.move('./drive/plugins', './')
+os.system('rm -rf ./drive')
+print('Optimized Plugins')
+ENV = os.environ.get("ENV", False)
+bool(ENV)
+basicConfig(format="◆━%(name)s━◆ ◤%(levelname)s◢ ║%(message)s║",level=INFO,)
+LOGS = getLogger(__name__)
+BOTLOG_CHATID = os.environ.get("BOTLOG_CHATID", None)
+try:
+    BOTLOG_CHATID = int(BOTLOG_CHATID)
+except:
+    pass
 
 bot2 = bot3 = bot4 = None
 if Var.STRING_SESSION:    
@@ -16,102 +28,49 @@ if Var.STR3:
 if Var.STR4:
     bot4 = TelegramClient(StringSession(Var.STR4),Var.APP_ID,Var.API_HASH,connection_retries=None,auto_reconnect=True,lang_code='en')
 
-shutil.move('./drive/plugins', './')
-os.system('rm -rf ./drive')
-print('Optimized Plugins')
-ENV = os.environ.get("ENV", False)
-if bool(ENV):
-    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+BOTLOG = sb(os.environ.get("BOTLOG", "False"))
+LOGSPAMMER = sb(os.environ.get("LOGSPAMMER", "False"))
+AUTOPIC_COMMENT = os.environ.get("AUTOPIC_COMMENT", "")
+AUTOPIC_FONT_COLOUR = os.environ.get("AUTOPIC_FONT_COLOUR", "")
+AUTOPIC_FONT = os.environ.get("AUTOPIC_FONT", "") 
+AUTO_BIO = os.environ.get("AUTO_BIO", "")
+PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
+CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+DB_URI = os.environ.get("DATABASE_URL", None)
+OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
+REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+MONGO_URI = os.environ.get("MONGO_URI", "")
+CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
+GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
+OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
+ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
+ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
+F_BAN_LOGGER_GROUP = os.environ.get("F_BAN_LOGGER_GROUP", None)
+HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
+YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
+ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
+AUTONAME = os.environ.get("AUTONAME", None)
+COUNTRY = str(os.environ.get("COUNTRY", "India"))
+TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
+FBAN_REASON = os.environ.get("FBAN_REASON", None)
+FBAN_USER = os.environ.get("FBAN_USER", None)
+CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
+CUSTOM_PMPERMIT = os.environ.get("CUSTOM_PMPERMIT", None)
+CUSTOM_STICKER_PACK_NAME = os.environ.get("CUSTOM_STICKER_PACK_NAME", None)
+CUSTOM_ANIMATED_PACK_NAME = os.environ.get("CUSTOM_ANIMATED_PACK_NAME", None)
+BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
+DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
+G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
+G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
+G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
+GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
+TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY","./downloads")
+LASTFM_API = os.environ.get("LASTFM_API", None)
+LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
+PLACEHOLDER = None
 
-    if CONSOLE_LOGGER_VERBOSE:
-        basicConfig(
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-            level=DEBUG,
-        )
-    else:
-        basicConfig(
-            format="◆━%(name)s━◆ ◤%(levelname)s◢ ║%(message)s║",
-            level=INFO,
-        )
-    LOGS = getLogger(__name__)
-
-    # Check if the config was edited by using the already used variable.
-    # Basically, its the 'virginity check' for the config file ;)
-    CONFIG_CHECK = os.environ.get(
-        "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
-
-    if CONFIG_CHECK:
-        LOGS.info(
-            "Please remove the line mentioned in the first hashtag from the config.env file"
-        )
-        quit(1)
-
-    # Logging channel/group configuration.
-    BOTLOG_CHATID = os.environ.get("BOTLOG_CHATID", None)
-    try:
-        BOTLOG_CHATID = int(BOTLOG_CHATID)
-    except:
-        pass
-
-    BOTLOG = sb(os.environ.get("BOTLOG", "False"))
-    LOGSPAMMER = sb(os.environ.get("LOGSPAMMER", "False"))
-    AUTOPIC_COMMENT = os.environ.get("AUTOPIC_COMMENT", "")
-    AUTOPIC_FONT_COLOUR = os.environ.get("AUTOPIC_FONT_COLOUR", "")
-    AUTOPIC_FONT = os.environ.get("AUTOPIC_FONT", "") 
-    AUTO_BIO = os.environ.get("AUTO_BIO", "")
-    PM_AUTO_BAN = sb(os.environ.get("PM_AUTO_BAN", "False"))
-    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
-    DB_URI = os.environ.get("DATABASE_URL", None)
-    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
-    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-    MONGO_URI = os.environ.get("MONGO_URI", "")
-    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
-    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
-    OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
-    ANTI_SPAMBOT = sb(os.environ.get("ANTI_SPAMBOT", "False"))
-    ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT", "False"))
-    F_BAN_LOGGER_GROUP = os.environ.get("F_BAN_LOGGER_GROUP", None)
-    HEROKU_MEMEZ = sb(os.environ.get("HEROKU_MEMEZ", "False"))
-    HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-    YOUTUBE_API_KEY = os.environ.get("YOUTUBE_API_KEY", None)
-    ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
-    AUTONAME = os.environ.get("AUTONAME", None)
-    COUNTRY = str(os.environ.get("COUNTRY", "India"))
-    TZ_NUMBER = int(os.environ.get("TZ_NUMBER", 1))
-    FBAN_REASON = os.environ.get("FBAN_REASON", None)
-    FBAN_USER = os.environ.get("FBAN_USER", None)
-    CLEAN_WELCOME = sb(os.environ.get("CLEAN_WELCOME", "True"))
-    CUSTOM_PMPERMIT = os.environ.get("CUSTOM_PMPERMIT", None)
-    CUSTOM_STICKER_PACK_NAME = os.environ.get("CUSTOM_STICKER_PACK_NAME", None)
-    CUSTOM_ANIMATED_PACK_NAME = os.environ.get("CUSTOM_ANIMATED_PACK_NAME", None)
-    BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
-    DEFAULT_BIO = os.environ.get("DEFAULT_BIO", None)
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET", None)
-    G_DRIVE_AUTH_TOKEN_DATA = os.environ.get("G_DRIVE_AUTH_TOKEN_DATA", None)
-    GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID", None)
-    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY","./downloads")
-    LASTFM_API = os.environ.get("LASTFM_API", None)
-    LASTFM_SECRET = os.environ.get("LASTFM_SECRET", None)
-    LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME", None)
-    LASTFM_PASSWORD_PLAIN = os.environ.get("LASTFM_PASSWORD", None)
-    LASTFM_PASS = pylast.md5(LASTFM_PASSWORD_PLAIN)
-    if not LASTFM_USERNAME == "None":
-        lastfm = pylast.LastFMNetwork(api_key=LASTFM_API,
-                                      api_secret=LASTFM_SECRET,
-                                      username=LASTFM_USERNAME,
-                                      password_hash=LASTFM_PASS)
-    else:
-        lastfm = None
-        
-    
-    
-    # Put your ppe vars here if you are using local hosting
-    PLACEHOLDER = None
-
-# Setting Up CloudMail.ru and MEGA.nz extractor binaries,
-# and giving them correct perms to work properly.
 if not os.path.exists('bin'):
     os.mkdir('bin')
 
