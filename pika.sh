7ea600820441841a50bacba8378deb64e1bf8e00
ItzSjDude <Support@itzsjdude.in>
2020-11-09 17:46:44 +0530

    PikaBot 2.1 Optimizations

---
+++ pika.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+
+echo '
+┏━┳┳┓╋╋╋┏┓╋╋┏┓           
+┃╋┣┫┣┳━┓┃┗┳━┫┗┓ •Deployment started•
+┃┏┫┃━┫╋┗┫╋┃╋┃┏┫
+┗┛┗┻┻┻━━┻━┻━┻━┛
+'
+export TZ=Asia/Kolkata
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+apt-get update 
+
+apt-get install -y \
+    coreutils \
+    bash \
+    nodejs \
+    bzip2 \
+    curl \
+    figlet \
+    gcc \
+    g++ \
+    git \
+    util-linux \
+    libevent-dev \
+    libjpeg-dev \
+    libffi-dev \
+    libpq-dev \
+    libwebp-dev \
+    libxml2 \
+    libxml2-dev \
+    libxslt-dev \
+    musl \
+    neofetch \
+    libcurl4-openssl-dev \
+    postgresql \
+    postgresql-client \
+    postgresql-server-dev-all \
+    openssl \
+    mediainfo \
+    wget \
+    python3 \
+    python3-dev \
+    python3-pip \
+    libreadline-dev \
+    zipalign \
+    sqlite \
+    ffmpeg \
+    libsqlite3-dev \
+    zlib1g-dev \
+    recoverjpeg \
+    zip \
+    megatools \
+    libfreetype6-dev \
+    procps \
+    policykit-1
+ 
+pip3 install --upgrade pip setuptools 
+git clone -b origin/Beta https://github.com/ItzSjDude/PikachuUserbot ./
+mkdir bin && mkdir pikabot/main_plugs
+pip3 install -r requirements.txt 
+
+echo '
+┏━┳┳┓╋╋╋┏┓╋╋┏┓         
+┃╋┣┫┣┳━┓┃┗┳━┫┗┓•Deployment Finished•
+┃┏┫┃━┫╋┗┫╋┃╋┃┏┫ Thank You For Deploying 
+┗┛┗┻┻┻━━┻━┻━┻━┛      PikachuUserbot 
+• Wait While image is being pushed to Heroku
+• Turn your Worker on 
+If You face any difficulties then contact @ItzSjDude 
+at @PikaUserbot_Support
+'
