c3d4309e8d8df77f646f92806010a084558d0f78
Sachin Jangir <support@ItzSjDude.in>
2020-11-18 17:13:44 +0530

    Update Deps.sh

--- init/Deps.sh
+++ init/Deps.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 insReq() {
+    echo '••• Installing Requirements •••'
     pip3 install -r $1/requirements.txt &> /dev/null
+    echo 'Installed Requirements 🚶'
 }
 
 upgradePip() {
+    echo '••• Updating Pip •••' 
     pip3 install -U pip &> /dev/null
+    echo 'Updated Pip 🚶'
 }
