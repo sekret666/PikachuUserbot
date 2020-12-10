828dfbfec5639147a09fab8b3987be5710d1d8d3
Sachin Jangir <support@ItzSjDude.in>
2020-12-10 13:53:28 +0530

    Update __init__.py

--- pikabot/Assistant/plugins/__init__.py
+++ pikabot/Assistant/plugins/__init__.py
@@ -11,4 +11,4 @@
 
 from plugins.__init__ import assistant_modules 
 
-admin = [_ban,_demote,_gadmin,_gmte,_gusers,_kick,_mute,_muter,_pin,_promote,_rmdacc,_setgpic,_unban,_ungmute,_unmute]
+admin = [_ban ,_demote, _gadmin, _gmte, _gusers, _kick, _mute, _muter, _pin, _promote, _rmdacc, _setgpic, _unban, _ungmute, _unmute]
