78980c04b6a39add2f41de0a054c7bf20175f948
Sachin Jangir <support@ItzSjDude.in>
2020-12-10 19:17:40 +0530

    Test🚶🤔

---
+++ pikabot/Assistant/plugins/eval.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020 by ItzSjDude@Github, < https://github.com/ItzSjDude/PikachuUserbot >.
+#
+# This file is part of < https://github.com/ItzSjDude/PikachuUserbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# 
+# Please see < https://github.com/ItzSjDude/PikachuUserbot/blob/master/LICENSE >
+#
+# All rights reserved 
+
+from . import _eval, pikatgbot
+
+@ItzSjDude(pika=True, pattern="eval)
+@pikatgbot('Owner')
+async def _(event):
+  await _eval(event)
