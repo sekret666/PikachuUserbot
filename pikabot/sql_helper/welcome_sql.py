c5cc2525be07d2a2c96cec4be6eaee1530bd00ed
Sachin Jangir <support@ItzSjDude.in>
2020-12-11 16:42:52 +0530

    Optimized welcome_sql 🤔🚶

--- pikabot/sql_helper/welcome_sql.py
+++ pikabot/sql_helper/welcome_sql.py
@@ -1,144 +1,58 @@
-# © 2020 PikaBot
+#!/usr/bin/env python3
 #
-# You may not use this file without proper authorship and consent from @ItzSjDudeSupport
+# Copyright (C) 2020 by ItzSjDude@Github, < https://github.com/ItzSjDude/PikachuUserbot >.
+#
+# This file is part of < https://github.com/ItzSjDude/PikachuUserbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# 
+# Please see < https://github.com/ItzSjDude/PikachuUserbot/blob/master/LICENSE >
+#
+# All rights reserved 
 #
 # Made by @ItzSjDude for Pikabot
 
-
-from sqlalchemy import BigInteger, Boolean, Column, String, UnicodeText
-from pikabot.sql_helper import SESSION, BASE
-
+from sqlalchemy import *
+from . import SESSION, BASE
 
 class Welcome(BASE):
     __tablename__ = "welcome"
     chat_id = Column(String(14), primary_key=True)
-    custom_welcome_message = Column(UnicodeText)
-    media_file_id = Column(UnicodeText)
-    should_clean_welcome = Column(Boolean, default=False)
-    previous_welcome = Column(BigInteger)
+    pika_id = Column(Numeric, primary_key=True)
+    cust_wc = Column(UnicodeText)
+    cl_wc = Column(Boolean, default=False)
+    prev_wc = Column(BigInteger)
+    mf_id = Column(UnicodeText)
 
-    def __init__(
-        self,
-        chat_id,
-        custom_welcome_message,
-        should_clean_welcome,
-        previous_welcome,
-        media_file_id=None,
-    ):
+    def __init__(self, chat_id, pika_id, cust_wc, cl_wc, prev_wc, mf_id=None):
         self.chat_id = chat_id
-        self.custom_welcome_message = custom_welcome_message
-        self.media_file_id = media_file_id
-        self.should_clean_welcome = should_clean_welcome
-        self.previous_welcome = previous_welcome
-
-
+        self.pika_id = pika_id
+        self.cust_wc = cust_wc
+        self.cl_wc = cl_wc
+        self.prev_wc = prev_wc 
+        self.mf_id = mf_id
+        
 Welcome.__table__.create(checkfirst=True)
 
+def add_welcome(chat_id, pika_id, cust_wc, cl_wc, prev_wc, mf_id):
+    add_wc = Welcome(chat_id, pika_id, cust_wc, cl_wc, prev_wc, mf_id)
+    SESSION.add(add_wc)
+    SESSION.commit()
 
-def get_current_welcome_settings(chat_id):
+def remove_welcome(chat_id):
+    rm_wc = SESSION.query(Welcome).get((str(chat_id), pika_id))
+    if rm_wc:
+        SESSION.delete(rem)
+        SESSION.commit()
+
+def upd_prev_welcome(chat_id, pika_id, prev_wc):
+    get = SESSION.query(Welcome).get(chat_id, pika_id)
+    get.prev_wc = prev_wc
+    SESSION.commit()
+
+def get_welcome(chat_id, pika_id):
     try:
-        return SESSION.query(Welcome).filter(Welcome.chat_id == str(chat_id)).one()
+        return SESSION.query(Welcome).filter((Welcome.chat_id == str(chat_id), Welcome.pika_id == pika_id)).one()
     except:
         return None
     finally:
         SESSION.close()
-
-
-def add_welcome_setting(
-    chat_id,
-    custom_welcome_message,
-    should_clean_welcome,
-    previous_welcome,
-    media_file_id
-):
-    # adder = SESSION.query(Welcome).get(chat_id)
-    adder = Welcome(
-        chat_id,
-        custom_welcome_message,
-        should_clean_welcome,
-        previous_welcome,
-        media_file_id
-    )
-    SESSION.add(adder)
-    SESSION.commit()
-
-
-def rm_welcome_setting(chat_id):
-    rem = SESSION.query(Welcome).get(str(chat_id))
-    if rem:
-        SESSION.delete(rem)
-        SESSION.commit()
-
-
-def update_previous_welcome(chat_id, previous_welcome):
-    row = SESSION.query(Welcome).get(chat_id)
-    row.previous_welcome = previous_welcome
-    # commit the changes to the DB
-    SESSION.commit()
-
-class Welcome2(BASE):
-    __tablename__ = "welcome2"
-    chat_id = Column(String(14), primary_key=True)
-    custom_welcome_message = Column(UnicodeText)
-    media_file_id = Column(UnicodeText)
-    should_clean_welcome = Column(Boolean, default=False)
-    previous_welcome = Column(BigInteger)
-
-    def __init__(
-        self,
-        chat_id,
-        custom_welcome_message,
-        should_clean_welcome,
-        previous_welcome,
-        media_file_id=None,
-    ):
-        self.chat_id = chat_id
-        self.custom_welcome_message = custom_welcome_message
-        self.media_file_id = media_file_id
-        self.should_clean_welcome = should_clean_welcome
-        self.previous_welcome = previous_welcome
-
-
-Welcome2.__table__.create(checkfirst=True)
-
-
-def get_current_welcome_settings2(chat_id):
-    try:
-        return SESSION.query(Welcome2).filter(Welcome2.chat_id == str(chat_id)).one()
-    except:
-        return None
-    finally:
-        SESSION.close()
-
-
-def add_welcome_setting2(
-    chat_id,
-    custom_welcome_message,
-    should_clean_welcome,
-    previous_welcome,
-    media_file_id
-):
-    # adder = SESSION.query(Welcome).get(chat_id)
-    adder = Welcome2(
-        chat_id,
-        custom_welcome_message,
-        should_clean_welcome,
-        previous_welcome,
-        media_file_id
-    )
-    SESSION.add(adder)
-    SESSION.commit()
-
-
-def rm_welcome_setting2(chat_id):
-    rem = SESSION.query(Welcome2).get(str(chat_id))
-    if rem:
-        SESSION.delete(rem)
-        SESSION.commit()
-
-
-def update_previous_welcome2(chat_id, previous_welcome):
-    row = SESSION.query(Welcome2).get(chat_id)
-    row.previous_welcome = previous_welcome
-    # commit the changes to the DB
-    SESSION.commit()
